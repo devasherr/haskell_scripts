b571f19de3097f5e26ed240b169bba541611ea57
Asher Samuel <ashersam116@gmail.com>
2025-07-12 19:22:41 +0300

    example of custom types

--- type.hs
+++ type.hs
@@ -84,3 +84,18 @@ treeElem x (Node a left right)
     | x == a = True
     | x < a = treeElem x left
     | x > a = treeElem x right
+
+data TrafficLight = Red | Yellow | Green
+instance Eq TrafficLight where
+    Red == Red = True
+    Green == Green = True
+    Yellow == Yellow = True
+    _ == _ = False
+instance Show TrafficLight where
+    show Red = "Red Light"
+    show Green = "Green Light"
+    show Yellow = "Yellow Light"
+
+-- lets implement class YesNo a where
+    yesno :: a -> Bool
+instance 
